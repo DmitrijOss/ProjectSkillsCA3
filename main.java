4a087fbd6c134ed8091ebaaf7f3f035c216bca79
b00096684 <33843523+b00096684@users.noreply.github.com>
2018-11-28 15:32:33 +0000

    Add files via upload
    testing colmmit

--- main.java
+++ main.java
@@ -1,5 +1,10 @@
+public class main{
+
 public static void main(String[] args){
  System.out.println("Hi if ur reading this then I got github working");
  
  System.out.println("Trying to create a new branch");
-}
+ 
+ System.out.println("Dillon edited this 28/11/18 15:17"); 
+	}//end main
+}//end class
