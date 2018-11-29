d2071426a916692432fb0dc148b08d69fe465923
b00096684 <33843523+b00096684@users.noreply.github.com>
2018-11-29 14:34:20 +0000

    Add files via upload
    GUI finished just need to display linked list in a label, add/remove need to be finished before that can be done

--- main.java
+++ main.java
@@ -4,70 +4,70 @@ import java.util.*;
 import javax.swing.*;
 
 public class main extends JFrame implements ActionListener {
-	
+
 	public main(){
-		
+
+		//GUI is done just the linked list needs to be displayed on the label
+
 		setTitle("Frame");
 		setSize(500,500);
-		
-		JButton addButton = new JButton("Add Button");
-		JButton removeButton = new JButton("Remove Button");
-		JButton searchButton = new JButton("Search Button");
-		JTextField textField = new JTextField(20);
-		
-		//textField.setPreferredSize(new Dimension(150, 30));
-		
-		JLabel label = new JLabel ("Search");
-		
-		
+
+		JButton addButton = new JButton("Add to List");
+		JButton removeButton = new JButton("Remove from List");
+		JButton searchButton = new JButton("Search in List");
+		JTextField textFieldAdd = new JTextField(25);
+		JTextField textFieldRemove = new JTextField(25);
+		JTextField textFieldSearch = new JTextField(25);
+
+		JLabel label = new JLabel ("List will appear here");
+
+
 		addButton.addActionListener(this);
 		removeButton.addActionListener(this);
 		searchButton.addActionListener(this);
-		
+
 		JPanel panel = new JPanel();
-		
+
+		panel.add(textFieldAdd);
 		panel.add(addButton);
+
+		panel.add(textFieldRemove);
 		panel.add(removeButton);
+
+		panel.add(textFieldSearch);
 		panel.add(searchButton);
-		panel.add(textField);
+
 		panel.add(label);
-		
+
 		this.getContentPane().add(panel);
-		
+
 		setVisible(true);
 		setResizable(false);
-		
-		String text = textField.getText();
-		if(text.isEmpty()) {
-			
-			label.setText("Field is empty");
-			
-		}//end if
-		
-		else {
-			
-			label.setText(text);
-			
-		}//end else
-		
+
+		//String text = <LINKEDLIST CONTENTS>
+		//
+		//need to convert the linked list to a string that can be displayed here
+		//
+		//label.setText(text);
+
 	}//end main
-	
+
 	public void actionPerformed(ActionEvent e) {
-		
+
 		System.out.println("button works");
-		
+
 	}//end action
 
 public static void main(String[] args){
-	
+
 	main main = new main();
-	
+
 	System.out.println("Hi if ur reading this then I got github working");
- 
+
 	System.out.println("Trying to create a new branch");
- 
-	System.out.println("Dillon edited this 28/11/18 15:17"); 
- 
+
+	System.out.println("Dillon edited this 28/11/18 15:17");
+
 	}//end main
 
 }//end class
